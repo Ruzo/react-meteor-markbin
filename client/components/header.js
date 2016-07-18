73470d84ff1063f5fda116d03a8b44e926b9fe3d
Ruzo <richard@evigy.com>
2016-07-18 09:48:59 -0400

    Basic react setup

---
+++ client/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <ul className="w3-navbar w3-blue-grey w3-large">
+      <li className="w3-yellow"><a href="#">MarkBin</a></li>
+      <li><a href="#">Sign Up</a></li>
+      <li><a href="#">Create Bin</a></li>
+    </ul>
+  );
+};
+
+export default Header;
