4376e57c2c6509ac63bc080e140e1c4610afa633
Ruzo <richard@evigy.com>
2016-08-05 12:26:30 -0400

    Accounts-ui to bootstrap. Added accounts config.

--- client/components/accounts.js
+++ client/components/accounts.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Template} from 'meteor/templating';
 import {Blaze} from 'meteor/blaze';
+import AccountsUI from 'meteor/ian:accounts-ui-bootstrap-3';
 
 class Accounts extends Component {
   componentDidMount(){
-    this.view = Blaze.render(Template.loginButtons, ReactDOM.findDOMNode(this.refs.container));
+    this.view = Blaze.render(Template._loginButtons, ReactDOM.findDOMNode(this.refs.container));
   }
 
   componentWillUnmount(){
